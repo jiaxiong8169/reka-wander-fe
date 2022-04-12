f768156bc3e1650e80fa96aeb996f88aa6ede000
Jia Xiong <chinxiongwei@yahoo.com.my>
2022-04-12 22:27:53 +0800

    Refine a bit of the login page design

--- app/containers/auth/SignInScreen.js
+++ app/containers/auth/SignInScreen.js
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import {useAuth} from '../../hooks/useAuth';
 import GoogleAuth from '../../components/GoogleAuth';
+import GradientBackground from '../../components/GradientBackground';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SignInScreen = ({navigation, route}) => {
   const {authData, signIn, setAuthError} = useAuth();
@@ -45,95 +47,100 @@ const SignInScreen = ({navigation, route}) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <Text style={styles.titleStyle}>Hello Traveller!</Text>
-      </View>
-      <View style={{flex: 1}}>
-        <Text style={styles.subtitleStyle}>
-          {"Welcome back.\nYou've been missed."}
-        </Text>
-      </View>
-      <View style={{flex: 2, justifyContent: 'center'}}>
-        <View style={styles.inputField}>
-          <View
-            style={{
-              backgroundColor: 'rgba(255,255,255,0.3)',
-              padding: 5,
-              fontSize: 40,
-            }}>
-            <View>
+    <LinearGradient
+      colors={['#CFDDFC', 'white']}
+      start={{x: 0, y: 0}}
+      end={{x: 0, y: 0.5}}
+      style={{height: '100%', width: '100%'}}>
+      <View style={styles.container}>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <Text style={styles.titleStyle}>Hello Traveller!</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={styles.subtitleStyle}>
+            {"Welcome back.\nYou've been missed."}
+          </Text>
+        </View>
+        <View style={{flex: 2, justifyContent: 'center'}}>
+          <View style={styles.inputField}>
+            <View
+              style={{
+                backgroundColor: 'rgba(255,255,255,0.3)',
+                padding: 5,
+                fontSize: 40,
+              }}>
+              <View>
+                <TextInput
+                  placeholder="E-mail"
+                  value={email}
+                  onChangeText={setEmail}
+                  placeholderTextColor="black"
+                  selectionColor={'black'}
+                  style={{color: 'black'}}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.inputField}>
+            <View
+              style={{
+                backgroundColor: 'rgba(255,255,255,0.3)',
+                padding: 5,
+              }}>
               <TextInput
-                placeholder="E-mail"
-                value={email}
-                onChangeText={setEmail}
-                placeholderTextColor="white"
-                selectionColor={'white'}
-                style={{color: 'white'}}
+                textContentType="password"
+                secureTextEntry={true}
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                placeholderTextColor="black"
+                selectionColor={'black'}
+                style={{color: 'black'}}
               />
             </View>
           </View>
         </View>
-        <View style={styles.inputField}>
-          <View
-            style={{
-              backgroundColor: 'rgba(255,255,255,0.3)',
-              padding: 5,
-              color: 'white',
+        <View styles={{flex: 1}}>
+          <View style={[styles.buttonContainer]}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleLoginButtonPress}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleRegisterButtonPress}>
+              <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+            <View style={[styles.otherMethod, {marginTop: 20}]}>
+              <View style={styles.continueWithText}>
+                <Text>Continue with</Text>
+              </View>
+              <View>
+                <GoogleAuth navigation={navigation} />
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={{flex: 1, alignSelf: 'center', marginTop: 20}}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate({name: 'MainScreen'});
             }}>
-            <TextInput
-              textContentType="password"
-              secureTextEntry={true}
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              placeholderTextColor="white"
-              style={{color: 'white'}}
-            />
-          </View>
+            <Text>Continue as Visitor</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      <View styles={{flex: 1}}>
-        <View style={[styles.buttonContainer]}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handleLoginButtonPress}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handleRegisterButtonPress}>
-            <Text style={styles.buttonText}>Register</Text>
-          </TouchableOpacity>
-          <View style={[styles.otherMethod, {marginTop: 20}]}>
-            <View style={styles.continueWithText}>
-              <Text>Continue with</Text>
-            </View>
-            <View>
-              <GoogleAuth navigation={navigation} />
-            </View>
-          </View>
-        </View>
-      </View>
-      <View style={{flex: 1, alignSelf: 'center', marginTop: 20}}>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate({name: 'MainScreen'});
-          }}>
-          <Text>Continue as Visitor</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-  titleStyle: {fontSize: 40, color: 'white', fontWeight: 'bold'},
-  subtitleStyle: {fontSize: 30, color: 'white'},
+  titleStyle: {fontSize: 30, fontWeight: 'bold'},
+  subtitleStyle: {fontSize: 20},
   shape: {
     height: 140,
     width: '100%',
-    backgroundColor: 'white',
     borderBottomRightRadius: 400,
     borderBottomLeftRadius: 400,
     alignSelf: 'center',
@@ -143,25 +150,20 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    padding: 30,
-    paddingTop: 0,
+    padding: 20,
     flexDirection: 'column',
-    backgroundColor: '#3986d4',
     justifyContent: 'center',
     alignItems: 'stretch',
   },
   inputField: {
-    borderColor: 'white',
     borderWidth: 4,
     marginBottom: 10,
     marginTop: 10,
     marginLeft: 5,
     marginRight: 5,
     borderRadius: 10,
-    textDecorationColor: 'white',
   },
   buttonContainer: {
-    // alignSelf: "center",
     margin: 2,
   },
   button: {
@@ -183,7 +185,7 @@ const styles = StyleSheet.create({
     padding: 3,
     alignItems: 'center',
   },
-  placeholder: {color: 'white'},
+  placeholder: {color: 'black'},
 });
 
 export default SignInScreen;
