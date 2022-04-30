47328112f5c16b084c059e79185a8fa7da3688ca
JBLew <autumnlewjb@gmail.com>
2022-04-30 11:11:11 +0800

    solve OTP backspace behaviour issue

--- app/containers/auth/OTPInput.js
+++ app/containers/auth/OTPInput.js
@@ -26,6 +26,7 @@ const OneDigitInput = React.forwardRef(
               keyboardType={'number-pad'}
               caretHidden
               ref={ref}
+              selection={{start: 1, end: 1}}
               onFocus={onFocus}
               onKeyPress={({nativeEvent}) => {
                 console.log(nativeEvent.key);
@@ -76,7 +77,6 @@ const OTPInput = ({setCode, editable}) => {
     if (text !== '')
       if (index < refArr.length - 1) refArr[index + 1].current.focus();
       else refArr[index].current.blur();
-    else if (index > 0) refArr[index - 1].current.focus();
   };
 
   useEffect(() => {
