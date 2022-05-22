8ca202a0203a9c918b7fe9177acf0788509b5024
JBLew <autumnlewjb@gmail.com>
2022-05-22 12:24:39 +0800

    Create edit profile screen
    Able to edit username, email and phone number

--- app/containers/settings/SettingsScreen.js
+++ app/containers/settings/SettingsScreen.js
@@ -1,17 +1,17 @@
 import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import {Button, View, Text} from 'native-base';
+import {Button, View, Text, Avatar} from 'native-base';
 import GradientBackground from '../../components/GradientBackground';
 import {useAuth} from '../../hooks/useAuth';
 import {useHttpCall} from '../../hooks/useHttpCall';
 import {preventBack} from '../../utils/navigation-utils';
 
-export const SettingsScreen = ({navigation}) => {
+export const MenuScreen = ({navigation}) => {
   const {authData, signOut} = useAuth();
   const {getWithAuth} = useHttpCall();
 
   useEffect(() => {
-    preventBack(navigation, 'Settings');
+    preventBack(navigation, 'Menu');
   }, [navigation]);
 
   const getProfile = () => {
@@ -26,20 +26,38 @@ export const SettingsScreen = ({navigation}) => {
   return (
     <GradientBackground>
       <View>
-        {!authData && (
+        {!authData ? (
           <Button
             style={styles.button}
             onPress={() => navigation.navigate('SignInScreen')}>
             Login
           </Button>
-        )}
-        {!!authData && (
+        ) : (
           <>
             <View>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}>
+                <Avatar
+                  bg="amber.400"
+                  src={{uri: authData?.profileSrc}}
+                  size="2xl">
+                  {authData?.name
+                    ?.split(' ')
+                    ?.slice(0, 2)
+                    ?.map(token => token.charAt(0))
+                    ?.join('')}
+                </Avatar>
+              </View>
               <Text>Name: {authData?.name ?? ''}</Text>
               <Text>Email: {authData?.email ?? ''}</Text>
             </View>
-            <Button style={styles.button} onPress={() => getProfile()}>
+            <Button
+              style={styles.button}
+              onPress={() => navigation.navigate('Profile')}>
               Profile
             </Button>
             <Button
@@ -63,5 +81,3 @@ const styles = StyleSheet.create({
     marginTop: 4,
   },
 });
-
-export default SettingsScreen;
