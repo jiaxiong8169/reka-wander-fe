91f21fbfc6bc2c98925057fac738b5c05f65e17f
Jia Xiong <wing8169@users.noreply.github.com>
2022-06-21 22:09:28 +0800

    Enable half star rating

--- app/components/RatingButton.js
+++ app/components/RatingButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import Stars from 'react-native-stars';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import StarRating from 'react-native-star-rating';
 
 TouchableOpacity.defaultProps = {activeOpacity: 0.7};
 
@@ -9,21 +10,21 @@ export const RatingButton = ({onPress, rating, editable, style}) => {
   // for selection
   if (editable) {
     return (
-      <Stars
-        default={rating}
-        update={onPress}
-        spacing={2}
-        count={5}
-        starSize={50}
-        fullStar={<Icon name={'star'} style={[styles.myStarStyle]} />}
-        emptyStar={
-          <Icon
-            name={'star-outline'}
-            style={[styles.myStarStyle, styles.myEmptyStarStyle]}
-          />
-        }
-        halfStar={<Icon name={'star-half'} style={[styles.myStarStyle]} />}
-        style={style}
+      <StarRating
+        halfStarEnabled
+        maxStars={5}
+        rating={rating}
+        selectedStar={onPress}
+        emptyStarColor="yellow"
+        fullStarColor="yellow"
+        halfStarColor="yellow"
+        starStyle={{
+          backgroundColor: 'transparent',
+          textShadowColor: 'black',
+          textShadowOffset: {width: 1, height: 1},
+          textShadowRadius: 2,
+        }}
+        starSize={15}
       />
     );
   }
@@ -31,6 +32,7 @@ export const RatingButton = ({onPress, rating, editable, style}) => {
   // for display
   return (
     <Stars
+      half
       default={rating}
       disabled
       spacing={2}
